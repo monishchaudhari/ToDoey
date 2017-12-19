2dd1d0fd89e2fb2ce3dc29d06fc1a0ab42b80203
Monish <monishchaudhari0@gmail.com>
2017-12-19 15:49:30 +0530

    Single view Totoey with Search Bar Complete

--- ToDoey/Controller/TodoListViewController.swift
+++ ToDoey/Controller/TodoListViewController.swift
@@ -7,20 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [Items]()
+    var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //connection with contecxt in app delegate
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        print(dataFilePath!)
-        
-        loadData()
+       loadData() //calling method
         
     }
     
@@ -31,15 +33,17 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)  //TOdoItemCell is identifier for cutom cell group
         
-        let item = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row] //retrive object value for [0/1/2...]
         
-        cell.textLabel?.text = item.title
+        cell.textLabel?.text = item.title //set lable text of cell
         
         //useing terenary operator
-        cell.accessoryType = item.done == true ? .checkmark : .none
+        cell.accessoryType = item.done == true ? .checkmark : .none //set cell checkmark
         
+        
+        //This comented code work same like upper line
         /* ^ use terenary instead
          if item.done == true {
          cell.accessoryType = .checkmark
@@ -50,15 +54,16 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
+  
     //MARK - Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        //set the checkmark value of object
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveData() //method call
         
-        saveData()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true) //to deselct as soon as we move our finget from row
     }
     
     
@@ -67,57 +72,101 @@ class TodoListViewController: UITableViewController {
         
         var textField =  UITextField()
         
-        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert) //generate alert box
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //What will happen when user click add item
             
+            //What will happen when user click add item
             if textField.text != "" {
-                let newItem = Items()
+                let newItem = Item(context: self.context) //object of Item using context
+              
+                //setting values of properties
                 newItem.title = textField.text!
+                newItem.done = false
                 
-                self.itemArray.append(newItem)
-                self.saveData()
-                
-                
+                self.itemArray.append(newItem) //appending to array
+                self.saveData() //method call
             }
         }
         
+        //alert which takes input from text field
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
         
         alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        
+        present(alert, animated: true, completion: nil) //perform action 
     }
     
+    //function which act like commit operation to the database
     func saveData() {
-        
-        let encoder = PropertyListEncoder()
-        do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+     do {
+            try context.save() //Save the data to database
         } catch {
-            print("Problem in encoder. \(error )")
+            print("Errors: \(error)")
         }
         
         tableView.reloadData()
     }
     
-    func loadData() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try  decoder.decode([Items].self, from: data)
-            } catch {
-                print("Error in Decoder: \(error)")
-            }
-            
+    
+    
+    //create fetch request function with type safety <Item> with default value
+    func loadData(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+            itemArray = try context.fetch(request) //Fetch Data from Database
+        } catch {
+            print("Error in fetching data: \(error)")
         }
+        tableView.reloadData()
         
     }
     
 }
 
+//MARK: - Extension by SearchBarDelegate
+
+extension TodoListViewController : UISearchBarDelegate {
+    
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        
+//        let request : NSFetchRequest<Item> = Item.fetchRequest()
+//
+//        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+//
+//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+//
+//        loadData(with: request)
+//
+//        }
+
+   
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            loadData()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        } else {
+            
+            let request : NSFetchRequest<Item> = Item.fetchRequest()
+            
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadData(with: request)
+
+        }
+     
+        
+        
+    }
+    
+    
+}
